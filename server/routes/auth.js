02dc0cc0def713f9d55e6a63c1106c05d1dea1fc
Somayeh5656 <@Somayeh5656>
2025-05-05 20:02:11 +0300

    routines

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,18 +1,40 @@
-const express= require('express');
-const router= express.Router();
-const User= require('../models/User')
+const express = require('express');
+const router = express.Router();
+const User= require('../models/User');
+const mongoose = require('mongoose');
+const bcrypt= require('bcryptjs');
+const jwt= require('jsonwebtoken');
+
+
+
+const JWT_SECRET= process.env.JWT_SECRET;
+
 
 router.post('/',async (req, res)=>{
 const {email, password} =req.body;
+
 try{
     const user= await User.findOne({email});
-    if(!user) return res.status(404).json({message:"User not found"})
-    if (user.password !== password){
+    if(!user) return res.status(404).json({message:"User not found"});
+
+    const isMatch= await bcrypt.compare( password, user.password);
+    if (!isMatch){
         return res.status(401).json({message:"Incorrect password"});
     }
-    res.status(200).json({message:"Login successful",user})
+
+    const token= jwt.sign(
+        {userId: user._id, email:user.email},
+        JWT_SECRET,
+        {expiresIn: '1h'}
+    );
+
+    res.status(200).json({message:"Login successful",token, user: {id: user._id, username: user.username, email: user.email}});
+
+
+
+
 }catch(err){
-    res.status(500).json({message:"Login error"})
+    res.status(500).json({message:"Login error"});
 
 }
 
