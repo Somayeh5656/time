9653fae51eb89c65746c7d6742e01168a8839b51
Somayeh5656 <@Somayeh5656>
2025-06-21 09:57:29 +0300

    new port for backend

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',  // Muista vaihtaa oikea URL tuotantoon
+  baseURL: 'https://time-1-1ewx.onrender.com/api',  // Muista vaihtaa oikea URL tuotantoon
 });
 
 export default instance;
